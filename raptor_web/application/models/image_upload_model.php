4cb53a7cccc2a5e9a122bb9eeec34d5aa3ea62df
Lucas PD <lucasdg92@gmail.com>
2012-11-14 10:34:27 -0200

    corrigindo a versao no git do image_upload_model que deve ser usado, plz façam commit de uma versao antiga por cima dessa neh

--- raptor_web/application/models/image_upload_model.php
+++ raptor_web/application/models/image_upload_model.php
@@ -6,47 +6,53 @@ class Image_upload_model extends CI_Model {
 		$this->load->database();
 	}
 
-	public function manage_image($nome, $description, $album_url, $direct_url)
+	public function manage_image($nome, $description, $album_url, $direct_url, $tags)
 	{
 
 		$resultado = array();
 				
-		//call scripts python
+		//TODO: call scripts python
 //		system('python C:\wamp\www\raptor-ibage-search\descriptor\desc-proj-norm\desc-proj-norm\caracterizar.py $upload_data['full_path'] vetor');
 //		system('python C:\wamp\www\raptor-ibage-search\descriptor\desc-proj-norm\desc-proj-norm\projeta.py '.$vetor.' C:\wamp\www\raptor-ibage-search\descriptor\desc-proj-norm\desc-proj-norm\dicionario-2000.pal vetorSaida.hist' );
 //		system('python C:\wamp\www\raptor-ibage-search\descriptor\desc-proj-norm\desc-proj-norm\normalizar.py '.$vetorSaida.' VetorSaidaTF.nor');
 		
-		//read NOR file to retrieve data
+		//TODO: read NOR file created to retrieve data
 		$filename = "C:/wamp/www/raptor-ibage-search/NOR/IMG000002.nor";
 		$handle = fopen($filename, "r");
-		$nor_raw_content = fread($handle, filesize($filename));
+		$descriptor = fread($handle, filesize($filename));
 		fclose($handle);
 		
-		//debug variable
-		$resultado['NOR'] = $nor_raw_content;
+		//debug variable --DELETE
+		$resultado['NOR'] = $descriptor;
 		
-		//get descripto. from NOR file?
-		$descriptor = '12132131,312,3,13,1,313131313,131435578,5324254668,434,3767857352,525376587235557795,635435768,56';
-		
-		//calculate md5
+		//TODO:calculate md5
 		$md5 = 'xs2x3x45dc6avsdbsydtyab78v90cwc';
 		
+		//NEW STEP
+		//TODO: calculate cluster --DONE?
+		$fk_cluster_id = $this->calculate_cluster($descriptor);
+		$resultado['clusters'] = $fk_cluster_id;
+		//REMOVE \/ when it works
+		$fk_cluster_id = 1;
+		
 		//insert image reference into table Images
-		$query = "INSERT INTO Images (md5, name, description, descriptor, album_url, direct_url) VALUES ('".$md5."', '".$nome."', '".$description."', '".$descriptor."', '".$album_url."', '".$direct_url."');";
+		$query = "INSERT INTO  `raptorsearch`.`images` (`md5`, `name`, `description`, `descriptor`, `fk_clusterID`, `album_url`, `direct_url`) 
+			VALUES ('".$md5."',  '".$nome."',  '".$description."',  '".$descriptor."', '".$fk_cluster_id."' ,  '".$album_url."',  '".$direct_url."');";
+		
 		$this->db->query($query);
 		
-		//debug variable
+		//debug variable --DELETE
 		$resultado['query1'] = $query;
 		
-		//the id o the inserted image
+		//the id of the inserted image
 		$image_id = mysql_insert_id();
 		
-		//debug variable
+		//debug variable --DELETE
 		$resultado['image_id'] = $image_id;
 		
-		//mount tag insertion query
+		//build tag insertion query
 		$values = array();
-		$tags = array('um', 'dois', 'tres', 'quatro');
+		$tags = explode(" ", $tags);
 		foreach ($tags as $item){
 			$values[] = "('".$item."', '".$image_id."')";
 		}
@@ -56,11 +62,55 @@ class Image_upload_model extends CI_Model {
 		//run tag insertion query
 		mysql_query($query);
 		
-		//debug variable
+		//debug variable --DELETE
 		$resultado['query2'] = $query;
-		
-		
 		
 		return $resultado;
 	}
+	
+	
+	private function calculate_cluster($originalNOR){
+		
+// 		$dicionario = 2000;
+		$insertedVector = $this->calculateFloatVector($originalNOR);
+		
+		$query = "SELECT * FROM  `clusters`";
+		$rs = $this->db->query($query);
+		$clusters = $rs->result_array();
+		
+		$nexterCluster = null;
+		$menorDistancia = 9999999999;
+		
+		foreach ($clusters as $key => $data){
+// 			$data = array contendo um campo centroid e um campo clusterID
+			$arrayCandidato = $this->calculateFloatVector($data['centroid']);
+			$distancia = $this->euclidean_canonical_very_dumb($arrayCandidato, $insertedVector);
+			
+			if ($distancia < $menorDistancia){
+				$menorDistancia = $distancia;
+				$nexterCluster = $data['clusterID'];
+			}
+		}
+		
+		return $nexterCluster;
+	}
+	
+	private function calculateFloatVector($NOR){
+		
+			$NOR = str_replace(",", ".", $NOR);
+			$array = str_split($NOR, 16);
+			return $array;
+	}
+	
+	private function euclidean_canonical_very_dumb($u, $v)
+	{
+		$distance = 0;
+		 
+		for ($i = 0; $i < 2000; $i++)
+			$distance += ($u[$i] - $v[$i]) * ($u[$i] - $v[$i]);
+			 
+			return $distance;
+	}
+	
+	
 }
