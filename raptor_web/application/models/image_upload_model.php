e8f82b7a135677751ab36125783a357d5abcf4ec
Fabio Hiroki <fabiothiroki@gmail.com>
2012-11-30 06:05:22 -0200

    insercao

--- raptor_web/application/models/image_upload_model.php
+++ raptor_web/application/models/image_upload_model.php
@@ -10,28 +10,51 @@ class Image_upload_model extends CI_Model {
 	{
 
 		$resultado = array();
+
+		$pythonpath = "c:\Python27\python.exe";
+
+		$command = $pythonpath.' assets\images\upload\caracterizar.py '.$upload_data['full_path'].' insert.key  ';
+		system($command);
+
+		$command2 = $pythonpath.' assets\images\upload\projetar.py assets\images\upload\insert.key assets\images\upload\dicionario-2000.pal  assets\images\upload\insertSaida';
+		system($command2);
+
+		$command3 = $pythonpath.' assets\images\upload\normalizar.py assets\images\upload\insertSaida.hist assets\images\upload\InsertSaidaTF';
+		system($command3);
+
+
 		
 		//TODO: (maybe) call scripts python separately
 //		system('python C:\wamp\www\raptor-ibage-search\descriptor\desc-proj-norm\desc-proj-norm\caracterizar.py $upload_data['full_path'] vetor');
 //		system('python C:\wamp\www\raptor-ibage-search\descriptor\desc-proj-norm\desc-proj-norm\projeta.py '.$vetor.' C:\wamp\www\raptor-ibage-search\descriptor\desc-proj-norm\desc-proj-norm\dicionario-2000.pal vetorSaida.hist' );
 //		system('python C:\wamp\www\raptor-ibage-search\descriptor\desc-proj-norm\desc-proj-norm\normalizar.py '.$vetorSaida.' VetorSaidaTF.nor');
 		
-		$python_script_path = "C:\wamp\www\raptor-ibage-search\raptor_web\assets\images\upload\TUDO.py";
+		//$python_script_path = "C:\wamp\www\raptor-ibage-search\raptor_web\assets\images\upload\TUDO.py";
 		$uploaded_image_path = $upload_data["full_path"];
 		//TODO: (maybe) ou se der pra pegar pelo nome do arquivo na mesma pasta sem o caminho
 		// $uploaded_image_path = $upload_data["file_path"];
-		$output_file = $upload_data["raw_name"].".nor";
+//		$output_file = $upload_data["raw_name"].".nor";
 		//TODO: really call python
 		// 		system('python '.$python_script_path.' '.$uploaded_image_path.' '.$output_file);
-		echo 'python '.$python_script_path.' '.$uploaded_image_path.' '.$output_file;
+		//echo 'python '.$python_script_path.' '.$uploaded_image_path.' '.$output_file;
 		
 		//$filename = "C:/wamp/www/raptor-ibage-search/raptor_web/assets/images/upload/".$output_file;
 		//TODO: remove MOCK
-		$filename = "C:/wamp/www/raptor-ibage-search/NOR/IMG000002.nor";
+		$filename = "assets\images\upload\InsertSaidaTF.nor";
 		$handle = fopen($filename, "r");
 		$descriptor = fread($handle, filesize($filename));
 		fclose($handle);
-		
+
+		$arraydesc = explode(" ",($descriptor));
+
+		foreach ($arraydesc as $key => $value) {
+			$arraydesc[$key] = number_format(floatval($value), 14, '.', '');
+		}
+
+		$arraydesc = array_slice($arraydesc, 0, 2000);
+
+		$descbd = implode("",$arraydesc);
+
 		//debug variable --DELETE OR COMMENT
 		//$resultado['NOR'] = $descriptor;
 		
@@ -41,7 +64,8 @@ class Image_upload_model extends CI_Model {
 		//$resultado['MD5'] = $md5;
 		
 		//calculate cluster -- TODO: DONE?
-		$fk_cluster_id = $this->calculate_cluster($descriptor);
+		$fk_cluster_id = $this->calculate_cluster($descbd);
+
 		//debug variable --DELETE OR COMMENT
 		$resultado['clusters'] = $fk_cluster_id;
 		//TODO: REMOVE \/ when it works, when tested if really works
@@ -50,7 +74,7 @@ class Image_upload_model extends CI_Model {
 		
 		//insert image reference into table Images
 		$query = "INSERT INTO  `raptorsearch`.`images` (`md5`, `name`, `description`, `descriptor`, `fk_clusterID`, `album_url`, `direct_url`) 
-			VALUES ('".$md5."',  '".$nome."',  '".$description."',  '".$descriptor."', '".$fk_cluster_id."' ,  '".$album_url."',  '".$direct_url."');";
+			VALUES ('".$md5."',  '".$nome."',  '".$description."',  '".$descbd."', '".$fk_cluster_id."' ,  '".$album_url."',  '".$direct_url."');";
 		
 		$this->db->query($query);
 		
@@ -71,8 +95,6 @@ class Image_upload_model extends CI_Model {
 		}
 		$line = implode(",", $values);
 		$query = "INSERT INTO Tags (pk_word, fk_image_id) VALUES ".$line." ;";
-		
-		//run tag insertion query
 		mysql_query($query);
 		
 		//debug variable --DELETE OR COMMENT
@@ -86,7 +108,7 @@ class Image_upload_model extends CI_Model {
 		
 // 		$dicionario = 2000;
 		$insertedVector = $this->calculateFloatVector($originalNOR);
-		
+
 		$query = "SELECT * FROM  `clusters`";
 		$rs = $this->db->query($query);
 		$clusters = $rs->result_array();
@@ -119,7 +141,7 @@ class Image_upload_model extends CI_Model {
 	{
 		$distance = 0;
 		 
-		for ($i = 0; $i < 2001; $i++)
+		for ($i = 0; $i < 2000; $i++)
 			$distance += ($u[$i] - $v[$i]) * ($u[$i] - $v[$i]);
 			 
 			return $distance;
